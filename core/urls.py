ec881958648a31e515960fae28b105d8be809464
Alexrovr <alexandr.v01@mail.ru>
2026-04-08 22:53:56 +0300

    update profile.html

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup, name='signup'),
-    path('profile/', views.profile, name='profile'),
+    path('profile/<int:user_id>/', views.profile, name='profile'),
 ]
